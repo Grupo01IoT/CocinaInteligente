9b722b75c56b0fe1a75a98ce9d3b5c2ff4189057
raul2222 <eaeaea7@gmail.com>
2019-12-11 02:52:59 +0100

    voz offline v2.1

--- Arduino/voice_offline_v2/demo.py
+++ Arduino/voice_offline_v2/demo.py
@@ -1,26 +1,34 @@
 import snowboydecoder
 import sys
 import signal
-from light import Light
 
 interrupted = False
+
 
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
+
 def interrupt_callback():
     global interrupted
     return interrupted
 
+if len(sys.argv) == 1:
+    print("Error: need to specify model name")
+    print("Usage: python demo.py your.model")
+    sys.exit(-1)
 
+model = sys.argv[1]
+
+# capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector("saved_model.pmdl", sensitivity=0.5, audio_gain=7)
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
 print('Listening... Press Ctrl+C to exit')
 
-led = Light(17)
-detector.start(detected_callback=led.blink,
+# main loop
+detector.start(detected_callback=snowboydecoder.play_audio_file,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
